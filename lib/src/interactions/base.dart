d69139264516f762d4a961081991c24cc2ec332a
Rodsevich <neton_12@hotmail.com>
2017-05-10 17:02:19 -0300

    interacciones

--- lib/src/interactions/base.dart
+++ lib/src/interactions/base.dart
@@ -1,3 +1,7 @@
+library interactions.backbone;
+
+import 'dart:async';
+
 /// Father class of every interaction the [Generator]s have with the user.
 abstract class Interaction {
   String message;
@@ -8,4 +12,16 @@ abstract class Interaction {
 }
 
 /// The entity in charge of instantiating and handling the [Interaction]s
-class InteractionsHandler {}
+abstract class InteractionsHandler {
+  IOStrategy ioStrategy;
+  InteractionsHandler(this.ioStrategy);
+  bool askForConfirmation(String message);
+  String askForInput(String message, String checkRegExp);
+  String askForselection(String message, List<String> options);
+}
+
+/// Father class of the interface to the IO operations with the user
+abstract class IOStrategy {
+  void print(String contents);
+  Stream<String> read;
+}
